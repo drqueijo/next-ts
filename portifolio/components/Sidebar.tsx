4913759e4cb09f0189e0da3492a777b37e7d6399
drqueijo <silveriomat1234@gmail.com>
2022-01-15 21:18:49 -0300

    feat: Basic implementation

---
+++ portifolio/components/Sidebar.tsx
@@ -0,0 +1,74 @@
+import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from "react-icons/ai";
+import { GiTie } from "react-icons/gi";
+import { GoLocation } from "react-icons/go";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+
+const Sidebar = () => {
+  const { theme, setTheme } = useTheme();
+
+  const changeTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <>
+      <img
+        src="https://media-exp1.licdn.com/dms/image/C4D03AQHR-LCGJIrISQ/profile-displayphoto-shrink_200_200/0/1585114845123?e=1647475200&v=beta&t=oiCHhoDwr4rZxHA88Iq7A4BAN4yYEKnAxPNn5UCqUPI"
+        alt="avatar"
+        className=" mx-auto border rounded-full "
+        height="128px"
+        width="128px"
+      />
+      <h3 className="my-4 text-3xl font-medium tracking-wider font-kaushan">
+        <span className="text-green ">Matheus</span> Silverio
+      </h3>
+      <p className="px-2 py-1 my-3 bg-gray-200 rounded-full dark:bg-dark-200 dark:bg-black-500">
+        Front end Developer
+      </p>
+      <a
+        href="/assets/Sumit Dey Resume.pdf"
+        download="Sumit Dey Resume.pdf"
+        className="flex items-center justify-center px-2 py-1 my-2 bg-gray-200 rounded-full cursor-pointer dark:bg-dark-200 dark:bg-black-500"
+      >
+        <GiTie className="w-6 h-6" />
+        <span>Download Resume</span>
+      </a>
+      <div className="flex justify-around w-9/12 mx-auto my-5 text-green md:w-full ">
+        <a href="https://www.linkedin.com/in/drqueijo/" >
+          <AiFillLinkedin className="w-8 h-8 cursor-pointer" />
+        </a>
+        <a href="https://github.com/drqueijo">
+          <AiFillGithub className="w-8 h-8 cursor-pointer" />{" "}
+        </a>
+      </div>
+      <div
+        className="py-4 my-5 bg-gray-200 dark:bg-dark-200 dark:bg-black-500"
+        style={{ marginLeft: "-1rem", marginRight: "-1rem" }}
+      >
+        <div className="flex items-center justify-center">
+          <GoLocation className="mr-2" /> <span>Umuarama, Brazil </span>
+        </div>
+        <p className="my-2 "> silveriomat1234@gmail.com </p>
+        <p className="my-2"> +55 44 9 9995 2330 </p>
+      </div>
+
+      {/* Email Button */}
+
+      <button
+        className="w-8/12 px-5 py-2 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-green to-blue-500 hover:scale-105 focus:outline-none"
+        onClick={() => window.open("mailto:silveriomat1234@gmail.com")}
+      >
+        Email me
+      </button>
+      <button
+        onClick={changeTheme}
+        className="w-8/12 px-5 py-2 my-4 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-green to-blue-500 focus:outline-none hover:scale-105 "
+      >
+        Toggle Theme
+      </button>
+    </>
+  );
+};
+
+export default Sidebar;
